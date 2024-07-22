ea54662fd7a05b72250a028de20eeda86559f3a0
Larissa <larissadesouzadossantossouzq@gmail.com>
2024-07-22 09:13:32 -0300

    22-07

---
+++ Fornecedor.cs
@@ -0,0 +1,6 @@
+public class Fornecedor
+{
+    string nome;
+    int64 id;
+    publics
+}
---
+++ LARIS/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LARIS;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
